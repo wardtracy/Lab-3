0c2840eec102b7b6da809d83ef11d4443690b9fd
Tracy Ward <wardt34@gmail.com>
2017-07-13 12:17:06 -0400

    added test comment

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
-
+//test
         int userNum = 0;
 
         System.out.println("Learn your squares and cubes!");
